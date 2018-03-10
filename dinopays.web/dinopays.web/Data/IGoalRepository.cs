13b8639afc34d999c4da5576e49b9b8bd91005e3
Adam Cox <adam@adamcox.me>
2018-03-10 21:11:12 +0000

    Adds Goals

---
+++ dinopays.web/dinopays.web/Data/IGoalRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using dinopays.web.Models;
+
+namespace dinopays.web.Data
+{
+    public interface IGoalRepository
+    {
+        IEnumerable<Goal> Goals { get; }
+    }
+}
