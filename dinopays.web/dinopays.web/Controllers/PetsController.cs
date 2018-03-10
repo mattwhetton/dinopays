6b4aaf434b0d50ba5e7e4f367416f04f945aa872
Adam Cox <adam@adamcox.me>
2018-03-10 13:20:42 +0000

    Adds Get Pet endpoint

---
+++ dinopays.web/dinopays.web/Controllers/PetsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using dinopays.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dinopays.web.Controllers
+{
+    [Route("api/[controller]")]
+    public class PetsController : Controller
+    {
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Pet Get(Guid id)
+        {
+            return new Pet
+            {
+                Id = id,
+                Health = 5
+            };
+        }
+    }
+}
