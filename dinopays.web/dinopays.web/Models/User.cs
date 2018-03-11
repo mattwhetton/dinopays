04ff733322996255f7efc9e28c507b97a55a8642
Adam Cox <adam@adamcox.me>
2018-03-11 00:32:35 +0000

    Registers Mongo

---
+++ dinopays.web/dinopays.web/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dinopays.web.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
--- dinopays.web/dinopays.web/Startup.cs
+++ dinopays.web/dinopays.web/Startup.cs
@@ -1,5 +1,7 @@
+using System;
 using dinopays.web.ApplicationServices;
 using dinopays.web.Data;
+using dinopays.web.Models;
 using dinopays.web.Options;
 using dinopays.web.Starling;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +9,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
 using Newtonsoft.Json.Converters;
 
 namespace dinopays.web
@@ -23,11 +30,20 @@ namespace dinopays.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureMongo();
+
             services.Configure<StarlingOptions>(Configuration);
 
             services.AddSingleton<IStarlingClient, StarlingClient>();
             services.AddSingleton<ISummaryBuilder, SummaryBuilder>();
             services.AddSingleton<IGoalRepository, GoalRepository>();
+            services.AddSingleton(p =>
+            {
+                var url = MongoUrl.Create("mongodb://localhost:27017/dinopays");
+                var client = new MongoClient(url);
+                return client.GetDatabase(url.DatabaseName);
+            });
+            services.AddSingleton(p => p.GetService<IMongoDatabase>().GetCollection<User>("user"));
 
             services.AddMvc().AddJsonOptions(o => o.SerializerSettings.Converters.Add(new StringEnumConverter()));
         }
@@ -61,6 +77,22 @@ namespace dinopays.web
                     name: "spa-fallback",
                     defaults: new { controller = "Home", action = "Index" });
             });
+        }
+
+        private static void ConfigureMongo()
+        {
+            var conventionPack = new ConventionPack
+            {
+                new CamelCaseElementNameConvention(),
+                new IgnoreIfNullConvention(true),
+                new IgnoreExtraElementsConvention(true),
+                new EnumRepresentationConvention(BsonType.String)
+            };
+            BsonSerializer.RegisterIdGenerator(typeof(Guid), GuidGenerator.Instance);
+
+            ConventionRegistry.Register("MongoConventions",
+                                        conventionPack,
+                                        t => t.Namespace != null && t.Namespace.StartsWith("dinopays.web"));
         }
     }
 }
